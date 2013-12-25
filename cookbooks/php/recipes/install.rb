0321191204413aa7ca0f439df238d84f358803ea
Jose Diaz-Gonzalez <myaccounts@savant.be>
2013-12-25 01:27:57 -0500

    Use ondrej PPA to install PHP 5.5

--- cookbooks/php/recipes/install.rb
+++ cookbooks/php/recipes/install.rb
@@ -1,3 +1,18 @@
+execute "request php55 key" do
+  command "gpg --keyserver keyserver.ubuntu.com --recv-keys E5267A6C"
+  not_if "gpg --list-keys E5267A6C"
+end
+
+execute "install php55 key" do
+  command "gpg -a --export E5267A6C | apt-key add -"
+  not_if "apt-key list | grep E5267A6C"
+end
+
+template "/etc/apt/sources.list.d/php55.list" do
+  source "etc/apt/sources.list.d/php55.list.erb"
+  notifies :run, "execute[apt-get update]", :immediately
+end
+
 [
   "php5",
   "php5-cli",
