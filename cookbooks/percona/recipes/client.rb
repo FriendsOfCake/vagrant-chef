b58dd5969e251c1e9a9ac0a7c343359196a019cb
AD7six <andydawson76@gmail.com>
2014-05-16 14:38:20 +0200

    upgrade to 5.6

--- cookbooks/percona/recipes/client.rb
+++ cookbooks/percona/recipes/client.rb
@@ -1,9 +1,9 @@
 include_recipe "percona::apt_repository"
 
-package "percona-server-common-5.5" do
+package "percona-server-common-5.6" do
   action :install
 end
 
-package "percona-server-client-5.5" do
+package "percona-server-client-5.6" do
   action :install
 end
