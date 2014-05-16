60f55e4e7e8ca5830aa95b0adafc3068c1846aed
Jose Diaz-Gonzalez <myaccounts@savant.be>
2014-05-16 01:55:19 -0400

    Split up gem installation so that it completes for users with slower networks

--- cookbooks/ruby/recipes/install.rb
+++ cookbooks/ruby/recipes/install.rb
@@ -13,9 +13,31 @@ execute "gem-update" do
   not_if { ::FileTest.exists?("/var/cache/.gem-updated") }
 end
 
-["heroku", "hub", "travis", "travis-lint"].each do |g|
-  execute "gem-install-#{g}" do
-    command "gem install #{g}"
-    not_if "gem list | grep #{g}"
+if platform_family?("debian")
+  execute "command -v heroku || wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh"
+else
+  execute "command -v heroku || wget -qO- https://toolbelt.heroku.com/install.sh | sh"
+end
+
+# TODO: Fix travis
+[
+  "hub",
+  "ffi",
+  "pusher-client",
+  "pry",
+  "launchy",
+  "highline",
+  "faraday",
+  "faraday_middleware",
+  "backports",
+  "addressable",
+  "gh",
+  "travis",
+  "travis-lint"
+].each do |g|
+  gem_package g do
+    gem_binary("/usr/bin/gem")
+    options "--no-ri --no-rdoc --verbose"
+    action :upgrade
   end
 end
