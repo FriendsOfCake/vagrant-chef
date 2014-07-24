67d1b41dd7f1f0345a596e3bca5ec7a3726371bc
Jose Diaz-Gonzalez <myaccounts@savant.be>
2014-07-24 01:06:14 -0400

    Create an env file in /etc/service-envs/app.env
    This can be used for command line tasks

--- cookbooks/cakephp/recipes/setup.rb
+++ cookbooks/cakephp/recipes/setup.rb
@@ -14,6 +14,14 @@ directory "/var/log/app" do
   action :create
 end
 
+directory "/etc/service-envs/" do
+  owner "vagrant"
+  group "www-data"
+  mode  0755
+  recursive true
+  action :create
+end
+
 node['cakephp']['versions'].each do | version, should_install |
   git "/var/virtual/cakephp-#{version}" do
     repository 'git://github.com/cakephp/cakephp.git'
@@ -21,4 +29,11 @@ node['cakephp']['versions'].each do | version, should_install |
     action :checkout
     only_if { should_install }
   end
+end
+
+template "/etc/service-envs/app.env" do
+  source "env.erb"
+  owner "vagrant"
+  group "www-data"
+  mode 0644
 end
